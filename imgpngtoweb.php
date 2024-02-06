647f92f7e7720c3c08d6610e4333d9f3a5cfbdc3
Personal <faisaltez@gmail.com>
2024-02-07 02:04:56 +0600

    image compressor

--- imgpngtoweb.php
+++ imgpngtoweb.php
@@ -1,25 +1,25 @@
 <?php
 
-function hs_png2webp($source_file, $destination_file, $compression_quality = 40)
+function hs_png2webp($source_file, $destination_file, $compression_quality = 50)
 {
     $image = imagecreatefrompng($source_file);
     imagepalettetotruecolor($image);
     imagealphablending($image, true);
     imagesavealpha($image, true);
-    imagewebp($image, $destination_file, 80);
+    imagewebp($image, $destination_file, $compression_quality);
     // if (false === $result) {
     //     return false;
     // }
-    // imagedestroy($image);
+    imagedestroy($image);
     return $destination_file;
 }
 
 
-// echo hs_png2webp('img/about-img.png','img/about-img.webp');
+// echo hs_png2webp('img/1.png','img/1.webp');
 
 
 
-function hs_jpg2webp($source_file, $destination_file, $compression_quality = 60)
+function hs_jpg2webp($source_file, $destination_file, $compression_quality = 30)
 {
     $image = imagecreatefromjpeg($source_file);
     imagepalettetotruecolor($image);
@@ -33,4 +33,21 @@ function hs_jpg2webp($source_file, $destination_file, $compression_quality = 60)
     return $destination_file;
 }
 
-echo hs_jpg2webp('img/doctor-01.jpg','img/doctor-01.webp');
+// echo hs_jpg2webp('img/bg.jpg', 'img/bg.webp');
+
+
+##################################
+######### Base64 #################
+##################################
+$file = 'client/assets/images/sliders/1.webp';
+$path = pathinfo($file);
+$ext  = mb_strtolower($path['extension']);
+
+if (in_array($ext, array('jpeg', 'jpg', 'gif', 'png', 'webp'))) {     
+    $size = getimagesize($file);  
+    $img = 'data:' . $size['mime'] . ';base64,' . base64_encode(file_get_contents($file));
+}
+
+?>
+<?php echo $img; ?>
+<!-- // <img src="<?php echo $img; ?>"> -->
